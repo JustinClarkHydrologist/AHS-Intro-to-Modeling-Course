81ea91f7f15246b08f9adfae09522f8b6ed820d8
Justin Allen Clark <hydrogeomodels@gmail.com>
2026-09-04 09:24:25 -0700

    Update batch script
     comments and formatting

--- MODFLOW_Examples/Example1/_RunMe_MODFLOW.bat
+++ MODFLOW_Examples/Example1/_RunMe_MODFLOW.bat
@@ -1,9 +1,10 @@
-: Lines that begin with ":" are ignored
-: Lines that begin with echo shown on screen
-: This is the Command Line script that is used to activate MODFLOW
+REM Lines that begin with "REM" or ":" are ignored
+REM Lines that begin with echo shown on screen
+REM This is the Command Line script that is used to activate MODFLOW
 echo Starting Model
+
 mf2005.exe example1.nam
+pause
+
 :MODFLOW-NWT.exe example1.nam
 :mf-owhm.exe example1.nam
-:
-pause
